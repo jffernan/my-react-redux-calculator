fb9e1f4cd584249b02146cbb68680fe0eacca920
jffernan <jffernan@comcast.net>
2017-11-23 17:12:12 -0800

    added calculator buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Buttons from './Buttons';
+import OutputForm from './OutputForm';
 
 class App extends Component {
   render() {
---
+++ src/components/Buttons.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const Buttons = () => {
+  return (
+    <div className = "buttons" >
+      <div className = "row1" >
+        <button className = "clear">A/C
+        </button>
+        <button className = "percent">%
+        </button>
+        <button className = "plusMinus">+/-
+        </button>
+        <button className = "divide">/
+        </button>
+      </div>
+      <div className = "row2" >
+        <button className = "seven">7
+        </button>
+        <button className = "eight">8
+        </button>
+        <button className = "nine">9
+        </button>
+        <button className = "multiply">/X
+        </button>
+      </div>
+      <div className = "row3" >
+        <button className = "four">4
+        </button>
+        <button className = "five">5
+        </button>
+        <button className = "six">6
+        </button>
+        <button className = "subtract">-
+        </button>
+      </div>
+      <div className = "row4" >
+        <button className = "one">1
+        </button>
+        <button className = "two">2
+        </button>
+        <button className = "three">3
+        </button>
+        <button className = "plus">+
+        </button>
+      </div>
+      <div className = "row5" >
+        <button className = "zero">0
+        </button>
+        <button className = "decimal">.
+        </button>
+        <button className = "equals">=
+        </button>
+        <button className = "enter">Enter
+        </button>
+      </div>
+    </div>
+  )
+};
+
+export default Buttons;
