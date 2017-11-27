6c7a4e4854d96478a4fac5c33edd0d12b26a4f06
James Fernandez <jffernan@comcast.net>
2017-11-27 12:23:59 -0800

    small edit to add 0 in front of decimal point.

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ export class App extends Component {
 						<div className = "row" >
 							{ numberButtons0 }
 							<Button
-								callback={ () => enterNumber( "." ) }
+								callback={ () => enterNumber( "0." ) }
 								value="."
 							/>
 							<Button
